5c83af0a18392242dfe1a5e23fc42124c1a4648e
Madsag89 <71001617+Madsag89@users.noreply.github.com>
2021-05-27 12:59:39 +0200

    Add files via upload

---
+++ yunik-typed.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed('.anim', {
+    strings: 
+    ['You',
+     'Yunik'],
+    
+     backSpeed: 150,
+     typeSpeed: 150,
+     loop: false,
+     smartBackspace: true,
+     startDelay: 500,
+     backDelay: 700,
+    });
+    
